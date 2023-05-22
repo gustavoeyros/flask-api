e9924ad830a17daf72d233a4c5aedfdbf0f46dfe
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-05-22 13:53:14 -0300

    feat: password encryption

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Dependencies:
 <li>flask</li>
 <li>pymongo</li>
 <li>marshmallow & flask_marshmallow</li>
+<li>bcrypt</li>
 
 </ul>
--- controllers/UserController.py
+++ controllers/UserController.py
@@ -2,6 +2,7 @@ from flask import request, jsonify
 from models.bd import db
 from schemas.schemas import UserSchema, LoginSchema, AnimalSchema
 from bson import ObjectId
+import bcrypt
 
 
 def signUp():
@@ -10,6 +11,9 @@ def signUp():
     if errors:
         return jsonify(errors), 400
     data = request.get_json()
+    password = data.get('password')
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    data['password'] = hashed_password
     collection = db['users']
     collection.insert_one(data)
     return jsonify({'message': 'Usuário cadastrado com sucesso!'})
@@ -23,17 +27,16 @@ def signIn():
     data = request.get_json()
     email = data.get('email')
     password = data.get('password')
-
-    # Verifique se o usuário existe no banco de dados
     collection = db['users']
-    user = collection.find_one({'email': email, 'password': password})
-    user['_id'] = str(user['_id'])
+    user = collection.find_one({'email': email})
     if user:
-        # Usuário encontrado, pode prosseguir com o login
-        return jsonify({'message': 'Login bem-sucedido!', 'user': user})
-    else:
-        # Usuário não encontrado
-        return jsonify({'error': 'Credenciais inválidas'}), 401
+        hashed_password = user.get('password').decode(
+            'utf-8')  # Converter de BinData para string
+        if bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8')):
+            user['_id'] = str(user['_id'])
+            return jsonify({'message': 'Login bem-sucedido!', 'userId': user['_id']})
+    # Usuário não encontrado ou credenciais inválidas
+    return jsonify({'error': 'Credenciais inválidas'}), 401
 
 
 def saveAnimal(userId):
