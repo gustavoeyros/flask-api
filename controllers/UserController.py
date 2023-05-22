3daf3e81e222b258b0829066e043ae828ed49ba5
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-05-22 13:27:10 -0300

    feat: findAnimals and saveAnimals routes

--- controllers/UserController.py
+++ controllers/UserController.py
@@ -1,6 +1,7 @@
 from flask import request, jsonify
 from models.bd import db
 from schemas.schemas import UserSchema, LoginSchema, AnimalSchema
+from bson import ObjectId
 
 
 def signUp():
@@ -26,34 +27,51 @@ def signIn():
     # Verifique se o usuário existe no banco de dados
     collection = db['users']
     user = collection.find_one({'email': email, 'password': password})
-
+    user['_id'] = str(user['_id'])
     if user:
         # Usuário encontrado, pode prosseguir com o login
-        return jsonify({'message': 'Login bem-sucedido!'})
+        return jsonify({'message': 'Login bem-sucedido!', 'user': user})
     else:
         # Usuário não encontrado
         return jsonify({'error': 'Credenciais inválidas'}), 401
 
 
-def saveAnimal():
+def saveAnimal(userId):
     animalSchema = AnimalSchema()
     errors = animalSchema.validate(request.json)
     if errors:
         return jsonify(errors), 400
 
     data = request.get_json()
-    user_id = data.get('user_id')
-    animal_data = data.get('animal')
-
+    user_id = ObjectId(userId)
     user_collection = db['users']
     user = user_collection.find_one({'_id': user_id})
 
     if user:
         animal_collection = db['animals']
         animal_collection.insert_one(
-            {'user_id': user_id, 'animal': animal_data})
+            {'user_id': user_id, 'animal': data})
 
         return jsonify({'message': 'Animal registrado com sucesso!'})
     else:
         # User not found
+        return jsonify({'error': 'Usuário não encontrado!'}), 404
+
+
+def findAnimals(userId):
+    user_id = ObjectId(userId)
+    user_collection = db['users']
+    user = user_collection.find_one({'_id': user_id})
+
+    if user:
+        animal_collection = db['animals']
+        animals = animal_collection.find({'user_id': user_id})
+
+        animal_list = []
+        for animal in animals:
+            animal_info = animal['animal']
+            animal_list.append(animal_info)
+
+        return jsonify({'animals': animal_list})
+    else:
         return jsonify({'error': 'Usuário não encontrado!'}), 404
--- routes/routes.py
+++ routes/routes.py
@@ -1,14 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from controllers import UserController
 
 routes = Blueprint('routes', __name__)
 
 
 @routes.route('/signup', methods=['POST'])
-def signup():
+def signUp():
     return UserController.signUp()
 
 
 @routes.route('/signin', methods=['POST'])
-def signin():
+def signIn():
     return UserController.signIn()
+
+
+@routes.route('/saveanimal/<userId>', methods=['POST'])
+def saveAnimal(userId):
+    return UserController.saveAnimal(userId)
+
+
+@routes.route('/findanimals/<userId>', methods=['GET'])
+def findAnimals(userId):
+    return UserController.findAnimals(userId)
